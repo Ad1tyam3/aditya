2e2662cb0c07fd06dc0a8694c7b30ccd4ac73631
Ad1tyam3 <48468171+Ad1tyam3@users.noreply.github.com>
2019-03-19 20:01:52 +0530

    Add files via upload

---
+++ EPAM/Calculator/CalculatorUnitTestCase.java
@@ -0,0 +1,14 @@
+/*
+* Unit test cases for Calculator
+* 
+* @author MEDAPATI ADITYA
+*/
+class CalculatorUnitTestCase{
+
+	public void testOperation() {
+		Calculator c = new Calculator();
+		assertEquals(10, c.Operation(6,4,"Addition"));
+		assertEquals(2, c.Operation(6,4,"Subtraction"));
+		assertEquals(24, c.Operation(6,4,"Multiplication"));
+	}
+}
